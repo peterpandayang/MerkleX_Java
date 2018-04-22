c0c4dc39da793cdf8341fab5f317ebb83e81520c
peterpandayang <byang14@dons.usfca.edu>
2018-04-21 21:07:53 -0700

    some ideas about level3

--- src/main/java/edu/usfca/cs/merklex/JSONTool.java
+++ src/main/java/edu/usfca/cs/merklex/JSONTool.java
@@ -1,11 +1,27 @@
 package edu.usfca.cs.merklex;
 
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class JSONTool {
 
-    public static void main(String[] args){
+    static class Query{
+        String[] queries;
+        int p;
+        int len;
+        public Query(String[] queries){
+            this.queries = queries;
+            len = queries.length;
+            p = 0;
+        }
+    }
+
+    static class Status{
+        int arrayElementSeen = 0;
+    }
+
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String line = "";
 
@@ -34,8 +50,44 @@ public class JSONTool {
             }
 
             // try to query...
+            String[] queries = query.split(".");
+
+            queryWhileParsing(file, new Query(queries));
 
         }
+    }
+
+    private static void queryWhileParsing(File file, Query query) throws IOException {
+
+        InputStream inputStream = new FileInputStream(file);
+        // the buffer size is statically configured to 10 MB
+        byte[] buffer = new byte[10 * 1024 * 1024];
+        BufferedInputStream bin = new BufferedInputStream(inputStream);
+        int byteread;
+        String data = null;
+        // level is ensured by the stack's size
+        Stack<Integer> stack = new Stack<>();
+        Status status = new Status();
+        while((byteread = bin.read(buffer)) != -1){
+            data = new String(buffer);
+            queryFromData(data, query, stack, status);
+        }
+    }
+
+    private static void queryFromData(String data, Query query, Stack<Integer> stack, Status status){
+        long i = 0;
+        long len = data.length();
+        /*
+        * My logic here is that firstly get the next query and keep checking
+        * p in the query stands for the level of the query; if the stack's size
+        * is the same as p, it means that the result could be found in the current chunk
+        * of data; if the query could be found, add the point p in query by one, otherwise
+        * just read the next data chunk.
+        *
+        * Also there could be some case that it is consuming the jsonarray and I use the
+        * status to update how many elements have been seen so far.
+        *
+        * */
     }
 
 }
